64eaa0ee9644d80245470ac40ab95621f6b0c12b
arturasVis <99223804+arturasVis@users.noreply.github.com>
2022-03-09 00:43:33 +0000

    Done

---
+++ GoogleApiTestForms/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleApiTestForms
+{
+    public partial class Form1 : Form
+    {
+        static private string path = "C:\\Users\\artel\\OneDrive\\Desktop\\C# Shit\\GoogleSheetsAPIA\\ConsoleApp1\\";
+        GoogleApi sheets=new GoogleApi("Desktop application 1","1fEX7dsW_gGZFugrq9IvH18IuGLP8c02rJRuzIFtYtWI", path);
+        public Form1()
+        {
+            InitializeComponent();
+            sheets.Authentication();
+            var sheet=sheets.ReadEntries("!A","E","openorder");
+            //sheets.CreateEntry();
+
+            var source = readCSV(path + "openorder.csv");
+            dataGridView1.DataSource = sheet;
+            
+
+            List<object> list = new List<object>();
+            var append = Compare(sheet,source);
+            int lastIndex=sheet.Rows.Count;
+            dataGridView2.DataSource = append;
+            for (int i=0;i< append.Rows.Count;i++)
+            {
+                for (int j=0;j< append.Columns.Count;j++)
+                {
+                    list.Add(append.Rows[i][j]);
+                }
+                sheets.UpdateSheet(list, "openorder", "!A", "E");
+                list.Clear();
+                index(lastIndex);
+                lastIndex++;
+            }
+            Application.Exit();
+        }
+        public void index(int lastIndex)
+        {
+            int j = lastIndex;
+            j++;
+            int cell = j + 1;
+            List<object> list = new List<object>(); list.Add(j);
+            sheets.UpdateSheet(list,"openorder","!A"+cell,"A"+cell);
+            list.Clear();
+        }
+        
+        public DataTable readCSV(string filePath)
+        {
+            var dt=new DataTable();
+            DataColumn column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.AutoIncrement = true;
+            column.AutoIncrementSeed = 1;
+            column.AutoIncrementStep = 1;
+            column.ColumnName = "Index #";
+            dt.Columns.Add(column);
+            dt.Columns.Add("Order #");
+            dt.Columns.Add("SKU");
+            dt.Columns.Add("QTY");
+            dt.Columns.Add("Channel");
+            foreach (var line in File.ReadLines(filePath))
+            {
+                string[] vs = line.Split(',');
+                dt.Rows.Add(null,vs[0].Trim('"'),vs[1].Trim('"'), vs[2].Trim('"'), vs[3].Trim('"'));
+            }
+            return dt;
+        }
+        private DataTable Compare(DataTable FirstDataTable,DataTable SecondDataTable)
+        {
+            DataTable dt= new DataTable();
+            dt.Columns.Add("Index #");
+            dt.Columns.Add("Order #");
+            dt.Columns.Add("SKU");
+            dt.Columns.Add("QTY");
+            dt.Columns.Add("Channel");
+            bool isDuplicate=false;
+            for (int i=0;i<SecondDataTable.Rows.Count;i++)
+            {
+                for (int j=0;j<FirstDataTable.Rows.Count;j++)
+                {
+                    
+                    if (SecondDataTable.Rows[i][1].ToString()==FirstDataTable.Rows[j][1].ToString())
+                    {
+                        isDuplicate=true;
+                    }
+                }
+                if (isDuplicate==false)
+                {
+                    dt.Rows.Add(null, SecondDataTable.Rows[i][1], SecondDataTable.Rows[i][2], SecondDataTable.Rows[i][3], SecondDataTable.Rows[i][4]);
+                }
+                isDuplicate = false;
+            }
+            
+            return dt;
+        }
+    }
+}
---
+++ GoogleApiTestForms/GoogleApi.cs
@@ -0,0 +1,90 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Data;
+
+namespace GoogleApiTestForms
+{
+
+    internal class GoogleApi
+    {
+        string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        private string ApplicationName;
+        private string spreadsheetId;
+        SheetsService service;
+        private string path;
+        UserCredential credential;
+        public GoogleApi(string ApplicationName, string spreadsheetId, string path)
+        {
+            this.spreadsheetId = spreadsheetId;
+            this.ApplicationName = ApplicationName;
+            this.path = path;
+        }
+        public void Authentication()
+        {
+            using (var stream =
+                new FileStream("cred.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            Console.WriteLine();
+        }
+        public DataTable ReadEntries(string start, string end, string sheet)
+        {
+
+            var range = $"{sheet}{start}:{end}";
+            var request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+            var dataTable = new DataTable();
+            var response = request.Execute();
+            var values = response.Values;
+            dataTable.Columns.Add(values[0][0].ToString());
+            dataTable.Columns.Add(values[0][1].ToString());
+            dataTable.Columns.Add(values[0][2].ToString());
+            dataTable.Columns.Add(values[0][3].ToString());
+            dataTable.Columns.Add(values[0][4].ToString());
+            for (int i=1;i<values.Count;i++)
+            {
+                dataTable.Rows.Add(values[i][0], values[i][1], values[i][2], values[i][3], values[i][4]);
+            }
+            return dataTable;
+        }
+        public void UpdateSheet(List<object> s1, string sheet, string start,string end)
+        {
+            var range = $"{sheet}{start}:{end}";
+            var valueRange = new ValueRange();
+            
+            valueRange.Values = new List<IList<object>> { s1 };
+
+            var appendRequest = service.Spreadsheets.Values.Append(valueRange, spreadsheetId, range);
+            appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+            var appendReponse = appendRequest.Execute();
+        }
+        
+
+
+    }
+}
+
